2dcc59e8368a79991048a2117fc393f1b4aeb972
Lennyy1337 <lennyygameplay@gmail.com>
2024-05-20 04:12:11 +0200

    add

---
+++ src/app/images/page.tsx
@@ -0,0 +1,153 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { downloadUrl } from "download.js";
+import { useRouter } from "next/navigation";
+import { Select, SelectSection, SelectItem } from "@nextui-org/select";
+import { categories } from "@/lib/categories";
+
+export default function Home() {
+  const [pics, setPics] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
+  const [clicked, setClicked] = useState<boolean>(false);
+  const [attempts, setAttempts] = useState<number>(0);
+  const [selectedCategory, setSelectedCategory] = useState<string>('animals.birb');
+
+  const router = useRouter();
+  useEffect(() => {
+    getPics();
+  }, []);
+
+  async function getPics() {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const url = `https://v2.yiff.rest/${selectedCategory}/?notes=disabled&amount=5`;
+      console.log(selectedCategory)
+      if (loading) {
+        return;
+      }
+      const response = await axios.get(url);
+      const data = response.data;
+      const fetchedPics = data.images.map((image: any) => image.url);
+      setPics(fetchedPics);
+    } catch (error) {
+      if (attempts >= 3) {
+        console.error("Error fetching pictures:", error);
+        setError("Failed to fetch images. Please try again.");
+        setPics([]);
+        return;
+      }
+      setAttempts(attempts + 1);
+      setTimeout(getPics, 2000);
+    } finally {
+      setAttempts(0);
+      setLoading(false);
+    }
+  }
+
+  function ImageCard({ url }: { url: string }) {
+    function downloadpic() {
+      downloadUrl(url, url);
+    }
+
+    async function getFullImage() {
+      const imageUrl = url;
+
+      const regex = /\/([^/]+)$/;
+
+      const match = imageUrl.match(regex);
+      console.log(imageUrl);
+      console.log(match);
+      const filenameWithExtension = match ? match[1] : null;
+
+      const filename = filenameWithExtension
+        ? filenameWithExtension.split(".")[0]
+        : null;
+
+      router.push("/images/" + filename);
+    }
+
+    return (
+      <div
+        className="w-80 h-80 bg-white rounded-xl m-5 transition-all ease-in-out hover:scale-105"
+        style={{
+          backgroundImage: `url(${url})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          transition: "transform 0.3s ease-in-out",
+        }}
+        onLoad={() => setImageLoaded(true)}
+      >
+        <button
+          onClick={getFullImage}
+          className="border-2 m-2 border-black rounded-xl p-2"
+        >
+          Full screen
+        </button>
+        {/* Content */}
+      </div>
+    );
+  }
+
+  function SkeletonLoader() {
+    return (
+      <div
+        className={`w-80 h-80 bg-gray-300 rounded-xl m-5 animate-pulse ${
+          imageLoaded ? "hidden" : ""
+        }`}
+      ></div>
+    );
+  }
+
+  async function handleRegenerateClick() {
+    if (clicked) {
+      return;
+    }
+    setLoading(true);
+    setClicked(true);
+    setTimeout(async () => {
+      await getPics();
+      setClicked(false);
+    }, 1000);
+  }
+
+  return (
+    <>
+      <div className="max-w-screen flex flex-col bg-black min-w-screen md:h-screen">
+        <div className="w-full text-center">
+          <h1 className="m-12 text-center text-bold text-3xl">Tactical furry explorer</h1>
+          <Select label="Category" className="dark w-1/2" defaultSelectedKeys={["animals.birb"]} onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setSelectedCategory(event.target.value)}>
+            {categories.map((category) => (
+              <SelectItem key={category.db} value={category.db} className="dark">
+                {category.name}
+              </SelectItem>
+            ))}
+          </Select>
+        </div>
+
+        <div className="flex flex-wrap justify-center">
+          {loading ? (
+            Array.from({ length: 5 }).map((_, index) => (
+              <SkeletonLoader key={index} />
+            ))
+          ) : error ? (
+            <div className="text-red-500">{error}</div>
+          ) : (
+            pics.map((url, index) => <ImageCard key={index} url={url} />)
+          )}
+        </div>
+        <button
+          onClick={handleRegenerateClick}
+          className="border-4 border-green-700 md:w-1/2 w-full rounded-xl shadow-sm text-center justify-center self-center py-2 hover:bg-green-900 transition-all ease-in-out"
+        >
+          Regenerate images
+        </button>
+      </div>
+    </>
+  );
+}
---
+++ src/lib/categories.ts
@@ -0,0 +1,122 @@
+export const categories = [
+    {
+        "name": "Animals > Birb",
+        "db": "animals.birb",
+        "sfw": true,
+        "count": 65
+    },
+    {
+        "name": "Animals > Blep",
+        "db": "animals.blep",
+        "sfw": true,
+        "count": 50
+    },
+    {
+        "name": "Animals > Dik Dik",
+        "db": "animals.dikdik",
+        "sfw": true,
+        "count": 20
+    },
+    {
+        "name": "Furry > Boop",
+        "db": "furry.boop",
+        "sfw": true,
+        "count": 36
+    },
+    {
+        "name": "Furry > Cuddle",
+        "db": "furry.cuddle",
+        "sfw": true,
+        "count": 42
+    },
+    {
+        "name": "Furry > Flop",
+        "db": "furry.flop",
+        "sfw": true,
+        "count": 4
+    },
+    {
+        "name": "Furry > Fursuit",
+        "db": "furry.fursuit",
+        "sfw": true,
+        "count": 297
+    },
+    {
+        "name": "Furry > Hold",
+        "db": "furry.hold",
+        "sfw": true,
+        "count": 39
+    },
+    {
+        "name": "Furry > Howl",
+        "db": "furry.howl",
+        "sfw": true,
+        "count": 11
+    },
+    {
+        "name": "Furry > Hug",
+        "db": "furry.hug",
+        "sfw": true,
+        "count": 73
+    },
+    {
+        "name": "Furry > Kiss",
+        "db": "furry.kiss",
+        "sfw": true,
+        "count": 31
+    },
+    {
+        "name": "Furry > Lick",
+        "db": "furry.lick",
+        "sfw": true,
+        "count": 18
+    },
+    {
+        "name": "Furry > Propose",
+        "db": "furry.propose",
+        "sfw": true,
+        "count": 17
+    },
+    {
+        "name": "Furry > Butts",
+        "db": "furry.butts",
+        "sfw": false,
+        "count": 249
+    },
+    {
+        "name": "Furry > Bulge",
+        "db": "furry.bulge",
+        "sfw": false,
+        "count": 484
+    },
+    {
+        "name": "Furry > Yiff > Andromorph",
+        "db": "furry.yiff.andromorph",
+        "sfw": false,
+        "count": 418
+    },
+    {
+        "name": "Furry > Yiff > Gay",
+        "db": "furry.yiff.gay",
+        "sfw": false,
+        "count": 3951
+    },
+    {
+        "name": "Furry > Yiff > Gynomorph",
+        "db": "furry.yiff.gynomorph",
+        "sfw": false,
+        "count": 267
+    },
+    {
+        "name": "Furry > Yiff > Lesbian",
+        "db": "furry.yiff.lesbian",
+        "sfw": false,
+        "count": 433
+    },
+    {
+        "name": "Furry > Yiff > Straight",
+        "db": "furry.yiff.straight",
+        "sfw": false,
+        "count": 618
+    }
+]
